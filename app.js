bcdd612165e68214aed972a35a8336f2b5ab4fd7
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-01-29 20:49:48 +0000

    Add static routes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var port = 5000;
 
 app.listen(port, (err) => console.log('running on port: ' + port));
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
