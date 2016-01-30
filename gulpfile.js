aba5fa9fd5a6049a2a606389f6c423826cfc761c
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-01-30 00:13:27 +0000

    gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -15,13 +15,22 @@ gulp.task('style', () => {
 
 gulp.task('inject', () => {
     var wiredep = require('wiredep').stream;
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
     };
 
+    var inject = require('gulp-inject');
+    var injectSrc = gulp.src(['./public/css/*.css',
+                              './public/js/*.js'], {read: false});
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views/'));
 });
