c589e86f767ef9760a312a066d12e505723a090a
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-01-30 00:26:13 +0000

    Add serve task
    This task restart server when there is a change

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'scr/**/*.js'];
 
@@ -33,4 +34,20 @@ gulp.task('inject', () => {
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views/'));
+});
+
+gulp.task('serve', ['style', 'inject'], () => {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', (ev) => {
+            console.log('Restarting...');
+        });
 });
