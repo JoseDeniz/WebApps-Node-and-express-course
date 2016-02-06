f5d24a309a854fcaeb5b865b27afbeda9f2dd812
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-02-06 21:36:37 +0000

    Set the view of a single book

--- src/routes/BookRouter.js
+++ src/routes/BookRouter.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+var url = 'mongodb://localhost:27017/WebApps-Node-and-express-course';
 
 var router = (nav) => {
 
     bookRouter.route('/')
         .get((req, res) => {
-            var url = 'mongodb://localhost:27017/WebApps-Node-and-express-course';
-            var books;
-
             mongodb.connect(url, (err, db) => {
                 var booksCollection = db.collection('books');
                 booksCollection.find({}).toArray((err, result) => {
@@ -24,13 +24,18 @@ var router = (nav) => {
 
     bookRouter.route('/:id')
         .get((req, res) => {
-            var id = req.params.id;
-            res.render('bookView',
-                {
-                    title: books[id].title,
-                    nav: nav,
-                    book: books[id]
+            var id = new ObjectId(req.params.id);
+            mongodb.connect(url, (err, db) => {
+                var booksCollection = db.collection('books');
+                booksCollection.findOne({_id: id} , (err, result) => {
+                    res.render('bookView',
+                        {
+                            title: 'Books',
+                            nav: nav,
+                            book: result
+                        });
                 });
+            });
         });
     return bookRouter;
 };
