6c67be1dce00d3c03365471b203bb367e942c78a
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-02-09 21:40:36 +0000

    Check user in the DB

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var mongodb = require('mongodb').MongoClient;
 
 module.exports = () => {
     passport.use(new LocalStrategy({
@@ -7,10 +8,20 @@ module.exports = () => {
             passwordField: 'password'
         },
         (username, password, done) => {
-            var user = {
-                username: username,
-                password: password
-            };
-            done(null, user);
+            var url = 'mongodb://localhost:27017/WebApps-Node-and-express-course';
+            mongodb.connect(url, (err, db) => {
+                var userCollection = db.collection('users');
+                userCollection.findOne(
+                    {
+                        username: username
+                    },
+                    (err, result) => {
+                        if (result.password === password) {
+                            done(null, result);
+                        } else {
+                            done(null, false, {message: 'Incorrect Password'});
+                        }
+                    });
+            });
         }));
 };
