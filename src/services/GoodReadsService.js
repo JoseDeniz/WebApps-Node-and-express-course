df9f4236ec929274f42293a687434c5bc65e487d
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-02-12 01:20:23 +0000

    Show the image and the description given by the api

--- src/services/GoodReadsService.js
+++ src/services/GoodReadsService.js
@@ -1,8 +1,27 @@
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser({explicitArray: false});
+
 var goodReadServices = () => {
 
-    var getBookById = (id, callback) => {
-
-        callback(null, {description: 'Our Description'});
+    var getBookById = (id, cb) => {
+        var options = {
+            host: 'www.goodreads.com',
+            path: '/book/show/656?format=xml&key=xkc9mCKeWN4pKV2g5ui6g'
+        };
+        var callback = (response) => {
+            var str = '';
+            response.on('data', (chunk) => {
+                str += chunk;
+            });
+            response.on('end', () => {
+                console.log(str);
+                parser.parseString(str, (err, result) => {
+                    cb(null, result.GoodreadsResponse.book);
+                });
+            });
+        };
+        http.request(options, callback).end();
     };
 
     return {
